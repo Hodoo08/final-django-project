b427293c72250f69a8ffb902a701dba5c3de6e14
Hodoo08 <hodo08@hotmail.co.uk>
2025-03-22 07:24:19 +0300

    added django messages alert

--- inventory/views.py
+++ inventory/views.py
@@ -11,6 +11,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserRegistorForm, InventoryItemForm
 from .models import InventoryItem, Category
+from django.contrib import messages
+from inventory_management.settings import LOW_QUANTITY
 # Create your views here.
 
 class Index(TemplateView):
@@ -23,7 +25,31 @@ class Dashboard(LoginRequiredMixin, View):
     
     def get(self, request):
         items = InventoryItem.objects.all().order_by('id')
-        return render(request, 'inventory/dashboard.html', {'items': items})    
+        
+        low_inventory =  InventoryItem.objects.filter(
+            user=self.request.user.id,
+            quantity__lt=LOW_QUANTITY
+        )
+        if low_inventory.count() > 0:
+            if low_inventory.count()  > 1:
+                messages.error(request, f'{low_inventory.count()} items are low in inventory')
+            else:
+                messages.error(request, f'{low_inventory.count()} item is low in inventory')    
+                
+        low_inventory_ids = InventoryItem.objects.filter(
+            user=self.request.user.id,
+            quantity__lt=LOW_QUANTITY
+        ).values_list('id', flat=True)
+        
+        return render(
+            request, 
+            'inventory/dashboard.html', 
+            {
+                'items': items, 
+                'low_inventory_ids': low_inventory_ids
+            }
+        )  
+      
 
 
 class SignUpView(View):
